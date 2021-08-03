0c50f50be1cbe0ac4436b185bd30c111d97664a3
Glitch (13-webvr) <none>
2021-08-03 22:18:44 +0000

    Version 1 changes

--- script.js
+++ script.js
@@ -3,3 +3,51 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+  
+
+AFRAME.registerComponent('disappear', {       
+  
+  init: function () {      
+  
+  }  
+
+});
+
+AFRAME.registerComponent('disappear', {
+  
+  init: function () {
+  
+    let disappearfunc = () => {
+      this.el.setAttribute("visible", false);
+    };
+    
+    this.el.addEventListener('click', disappearfunc);
+    
+  }
+  
+ });
+
+// Simple example function: 
+let addingfunction = (x, y) => {
+  return x + y;   // The function returns the product of x and y
+}
+
+console.log(addingfunction(3, 5)); //console will log 8
+
+AFRAME.registerComponent('sphereexpand', {
+    
+   init: function () {
+
+      let homeworldelements = document.querySelectorAll(".homeworld");
+      let sky = document.querySelector("#sky");
+
+      let sphereloader = () => {
+      sky.setAttribute("src", "#360 photo 2");
+      homeworldelements.forEach((homeworldelement) => {
+      homeworldelement.setAttribute("visible", false)})
+      }
+
+      this.el.addEventListener('click', sphereloader);
+        
+   }});
