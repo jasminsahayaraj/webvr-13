5957beb439742cc37a63aa84fbdaa55d19cc8e24
Glitch (pro-13) <none>
2021-08-03 22:09:47 +0000

    Version 2

--- public/js/disappear.js
+++ public/js/disappear.js
@@ -1,2 +1,14 @@
+AFRAME.registerComponent('disappear', {
   
+  init: function () {
+  
+    let disappearfunc = () => {
+      this.el.setAttribute("visible", false);
+    };
+    
+    this.el.addEventListener('click', disappearfunc);
+    
+  }
+  
+ });  
  
--- public/js/sphereexpand.js
+++ public/js/sphereexpand.js
@@ -1,3 +1,19 @@
+AFRAME.registerComponent('sphereexpand', {
+    
+   init: function () {
+
+      let homeworldelements = document.querySelectorAll(".homeworld");
+      let sky = document.querySelector("#sky");
+
+      let sphereloader = () => {
+      sky.setAttribute("src", "#360 photo 2");
+      homeworldelements.forEach((homeworldelement) => {
+      homeworldelement.setAttribute("visible", false)})
+      }
+
+      this.el.addEventListener('click', sphereloader);
+        
+   }});
 
 
 
