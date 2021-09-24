48c2758a6727378bfde6d56f23f346a5a075b832
Glitch (13-webvr) <none>
2021-09-24 01:30:05 +0000

    Second page

--- public/js/sphereexpand.js
+++ public/js/sphereexpand.js
@@ -1,21 +1,3 @@
-AFRAME.registerComponent('sphereexpand', {
-    
-   init: function () {
-
-      let homeworldelements = document.querySelectorAll(".homeworld");
-      let sky = document.querySelector("#sky");
-
-      let sphereloader = () => {
-      sky.setAttribute("src", "#Entrance school 1");
-      homeworldelements.forEach((homeworldelement) => {
-      homeworldelement.setAttribute("visible", false)})
-      }
-
-      this.el.addEventListener('click', sphereloader);
-        
-   }});
-
-
 <!--photosphere-->
 AFRAME.registerComponent('sphereexpand', {
     
