d6b65997d1c3ac69b1e5746c52de6cb7a604ceea
Glitch (13-webvr) <none>
2021-09-29 23:27:58 +0000

    Second page

--- public/js/nextpage.js
+++ public/js/nextpage.js
@@ -1,19 +1,3 @@
-   AFRAME.registerComponent('nextpage', {
- 
-    
-   init: function () {
-     
-      let homeworldelements = document.querySelectorAll(".homeworld");
-      let sky = document.querySelector("#HVHS Library");
-      
-      let gonextpage = () => {
-      sky.setAttribute("src", "# HVHS Library");
-      homeworldelements.forEach((homeworldelement) => {
-      homeworldelement.setAttribute("visible", true)})
-    }
-
-      this.el.addEventListener('click', gonextpage);
-        
-   }});
+   
 
 
